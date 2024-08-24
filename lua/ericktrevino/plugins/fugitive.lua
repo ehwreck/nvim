9644530d1e84bb81abfe355f36a384671a4c37fe
Erick Trevino <erick.trevino@cloudlite.io>
2024-08-23 21:47:49 -0500

    Update fugitive config file.

--- lua/ericktrevino/plugins/fugitive.lua
+++ lua/ericktrevino/plugins/fugitive.lua
@@ -1,38 +1,42 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        -- Key mapping for Git status
+        vim.keymap.set("n", "<leader>gs", ":Git<CR>")
 
-        local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+        -- Autocommands group
+        local ErickTrevino_Fugitive = vim.api.nvim_create_augroup("ErickTrevino_Fugitive", {})
 
-        local autocmd = vim.api.nvim_create_autocmd
-        autocmd("BufWinEnter", {
-            group = ThePrimeagen_Fugitive,
+        -- BufWinEnter autocommand
+        vim.api.nvim_create_autocmd("BufWinEnter", {
+            group = ErickTrevino_Fugitive,
             pattern = "*",
             callback = function()
-                if vim.bo.ft ~= "fugitive" then
+                if vim.bo.filetype ~= "fugitive" then
                     return
                 end
 
                 local bufnr = vim.api.nvim_get_current_buf()
-                local opts = {buffer = bufnr, remap = false}
+                local opts = { buffer = bufnr, remap = false }
+
+                -- Git push
                 vim.keymap.set("n", "<leader>p", function()
                     vim.cmd.Git('push')
                 end, opts)
 
-                -- rebase always
+                -- Git pull with rebase
                 vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({'pull',  '--rebase'})
+                    vim.cmd.Git({ 'pull', '--rebase' })
                 end, opts)
 
-                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-                -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+                -- Git push with tracking branch
+                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
             end,
         })
 
-
+        -- Diffget key mappings
         vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
         vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
     end
 }
+
